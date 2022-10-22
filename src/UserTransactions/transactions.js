7fc5850a370e17f4f5e11d180e91fa7bec8a6a75
Harshita Bhatt <95317276+harshitab07@users.noreply.github.com>
2022-10-22 13:11:40 +0530

    Added UserTransactions file

---
+++ src/UserTransactions/transactions.js
@@ -0,0 +1,46 @@
+const transactions = [
+    {
+        date: "12-03-2021",
+        time: "13:00 hrs",
+        amount: "Rs.15000",
+        accountNo: 234567891262
+    },
+    {
+        date: "21-07-2021",
+        time: "08:00 hrs",
+        amount: "Rs.26000",
+        accountNo: 234567891262
+    },
+    {
+        date: "08-04-2022",
+        time: "11:00 hrs",
+        amount: "Rs.33000",
+        accountNo: 234567891462
+    },
+    {
+        date: "08-04-2022",
+        time: "11:00 hrs",
+        amount: "Rs.33000",
+        accountNo: 234567891462
+    },
+    {
+        date: "08-04-2022",
+        time: "11:00 hrs",
+        amount: "Rs.33000",
+        accountNo: 234567891462
+    },
+    {
+        date: "08-04-2022",
+        time: "11:00 hrs",
+        amount: "Rs.33000",
+        accountNo: 234567891462
+    },
+    {
+        date: "08-04-2022",
+        time: "11:00 hrs",
+        amount: "Rs.33000",
+        accountNo: 234567891462
+    },
+]
+
+export default transactions;
